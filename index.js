3e760da4879e760c26c6ada5dbe0b42d0b6ce259
Kai Sawamoto <kaisawamoto@gmail.com>
2020-03-09 23:00:03 -0700

    fixed index page

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const dotenv = require('dotenv');
 app.use(express.json());
 
 dotenv.config();
-
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 const PORT = 2000;
 
+const con = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
+});
+
 app.get('/', (req, res) => {
-	res.send('hello world');
+	res.render('home');
 });
 
 app.get('/api/user', (req, res) => {
@@ -20,7 +28,15 @@ app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
 });
 
-app.post('/animal/new', (req, res) => {});
+app.post('/animal/new', (req, res) => {
+	const { name, age, kind } = req.body;
+	const q = `INSERT INTO animal (name, age, kind) VALUES ("${name}", ${age}, "${kind}")`;
+	con.query(q, (error, result) => {
+		if (error) throw error;
+		console.log(result);
+	});
+	// res.status(200).send('Happy');
+});
 
 app.get('/animal', (req, res) => {
 	con.query('SELECT * FROM animal', (error, results, fields) => {
